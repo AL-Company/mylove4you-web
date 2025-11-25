066a336205cc463910d824a09477e8cb07aaf068
Ederson <edersonspt@gmail.com>
2025-11-25 17:58:06 -0300

    Update Width in BtnLink

--- src/components/shared/BtnLink.tsx
+++ src/components/shared/BtnLink.tsx
@@ -2,12 +2,20 @@
 interface BtnLinkProps {
     href: string;
     text: string;
+    width: number;
     className?: string;
 }
 
-export const BtnLink = ({ href, text, className = "BntLink " }: BtnLinkProps) => {
+export const BtnLink = ({ href, text, className = "BntLink ", width, ...props }: BtnLinkProps) => {
     return (
-        <a href={href} className={`px-6 py-3 rounded-full outline-none relative overflow-hidden border bg-cta cursor-pointer transform transition duration-300 hover:scale-102 ${className}`}>
-            <span className="relative z-10 text-white">{text}</span></a>
+        <a href={href}
+            style={{ width }}
+            className={`
+            px-6 py-3 rounded-full outline-none relative overflow-hidden border bg-cta cursor-pointer transform transition duration-300 hover:scale-102  ${className}`}
+            {...props}
+            >
+            
+            <span className="flex align-center justify-center relative z-10 text-white">{text}</span>
+        </a>
     );
 }
